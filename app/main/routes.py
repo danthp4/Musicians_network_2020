1a31f73857fd5a683592b6ed067023c1f4c2bd65
dillonlad <56037786+dillonlad@users.noreply.github.com>
2020-03-13 16:00:41 +0000

    about pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,7 @@ from app import db, login_manager
 from flask_login import login_required, current_user
 
 bp_main = Blueprint('main', __name__)
-
+bp_about = Blueprint('about', __name__, url_prefix='/about')
 
 @bp_main.route('/')
 def index():
@@ -14,6 +14,20 @@ def index():
     else:
         return render_template('index.html')
 
+
+@bp_about.route('/musicians')
+def musicians():
+    return render_template('about_musicians.html')
+
+
+@bp_about.route('/bands')
+def bands():
+    return render_template('about_bands.html')
+
+
+@bp_about.route('/venues')
+def venues():
+    return render_template('about_venues.html')
 
 
 @bp_main.route('/search', methods=['POST', 'GET'])
