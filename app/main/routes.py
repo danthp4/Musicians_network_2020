dcb39d3e3a78c8aca4dbbd7a348768f8f101ed6a
Dan Thompson <danielt397@gmail.com>
2020-03-28 20:12:11 +0000

    Merging error fix for Venues.html and venue_profile.html with main routes edit

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from app.models import Profile, Profile_Genre, Genre, Musician, Venue, Administrator, Media
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
 
 from app import db
+from app.models import Profile, Profile_Genre, Genre, Musician, Venue, Administrator, Media
 from app.prof.forms import RatingForm
 
 bp_main = Blueprint('main', __name__)
@@ -13,31 +13,31 @@ bp_about = Blueprint('about', __name__, url_prefix='/about')
 @bp_main.route('/', methods=['GET', 'POST'])
 def index():
     if current_user.is_authenticated:
-        admin = Administrator.query.join(Profile).filter(Administrator.profile_id == 
-            current_user.profile_id).first()
+        admin = Administrator.query.join(Profile).filter(Administrator.profile_id ==
+                                                         current_user.profile_id).first()
         form = RatingForm()
         # show non-blocked musicians only for users
         if admin is None:
             profiles = Profile.query.join(Musician).filter(Musician.profile_id != current_user.profile_id
-                                                    , Profile.block == 0).with_entities(Profile.username,
-                                                                                        Profile.location,
-                                                                                        Profile.profile_id,
-                                                                                        Profile.rating,
-                                                                                        Musician.sc_id,
-                                                                                        Profile.profile_description,
-                                                                                        Profile.block)
+                                                           , Profile.block == 0).with_entities(Profile.username,
+                                                                                               Profile.location,
+                                                                                               Profile.profile_id,
+                                                                                               Profile.rating,
+                                                                                               Musician.sc_id,
+                                                                                               Profile.profile_description,
+                                                                                               Profile.block)
         else:
             profiles = Profile.query.join(Musician).filter(Musician.profile_id != current_user.profile_id
-                                                                        ).with_entities(Profile.username,
-                                                                                        Profile.location,
-                                                                                        Profile.profile_id,
-                                                                                        Profile.rating,
-                                                                                        Musician.sc_id,
-                                                                                        Profile.profile_description,
-                                                                                        Profile.block)
+                                                           ).with_entities(Profile.username,
+                                                                           Profile.location,
+                                                                           Profile.profile_id,
+                                                                           Profile.rating,
+                                                                           Musician.sc_id,
+                                                                           Profile.profile_description,
+                                                                           Profile.block)
         relations = Profile_Genre.query.filter(Profile_Genre.profile_id != current_user.profile_id).all()
         genres = Genre.query.all()
-        
+
         user = Profile.query.filter_by(profile_id=current_user.profile_id).first()
         form.rating.data = user.rating
         if request.method == 'POST' and form.validate():
@@ -56,16 +56,27 @@ def index():
 @bp_main.route('/venues', methods=['GET', 'POST'])
 @login_required
 def venues():
-    admin = Administrator.query.join(Profile).filter(Administrator.profile_id == 
-        current_user.profile_id).first()
+    admin = Administrator.query.join(Profile).filter(Administrator.profile_id ==
+                                                     current_user.profile_id).first()
     if admin is None:
         profiles = Profile.query.join(Venue).filter(Venue.profile_id != current_user.profile_id,
-            Profile.block == 0).with_entities(Venue.venue_capacity, Profile.username, Profile.location, 
-            Profile.rating, Profile.profile_description, Profile.profile_id, Venue.venue_type, Profile.block)
+                                                    Profile.block == 0).with_entities(Venue.venue_capacity,
+                                                                                      Profile.username,
+                                                                                      Profile.location,
+                                                                                      Profile.rating,
+                                                                                      Profile.profile_description,
+                                                                                      Profile.profile_id,
+                                                                                      Venue.venue_type, Profile.block,
+                                                                                      Venue.venue_id,
+                                                                                      Profile.profile_image)
     else:
         profiles = Profile.query.join(Venue).filter(Venue.profile_id != current_user.profile_id
-            ).with_entities(Venue.venue_capacity, Profile.username, Profile.location, 
-            Profile.rating, Profile.profile_description, Profile.profile_id, Venue.venue_type, Profile.block)
+                                                    ).with_entities(Venue.venue_capacity, Profile.username,
+                                                                    Profile.location,
+                                                                    Profile.rating, Profile.profile_description,
+                                                                    Profile.profile_id, Venue.venue_type, Profile.block,
+                                                                    Venue.venue_id, Profile.profile_image)
+
     relations = Profile_Genre.query.filter(Profile_Genre.profile_id != current_user.profile_id).all()
     genres = Genre.query.all()
     media = Media.query.join(Venue).filter(Venue.profile_id != current_user.profile_id).all()
@@ -97,9 +108,11 @@ def bands():
 def venues():
     return render_template('about_venues.html')
 
+
 @bp_main.route('/soundcloud_id')
 def soundcloud_id():
     return render_template('soundcloud_id.html')
+
 
 @bp_main.route('/youtube_id')
 def youtube_id():
@@ -131,11 +144,12 @@ def search_results(search):
     else:
         return redirect(url_for('main.index'))
 
+
 @bp_main.route('/block/<username>')
 @login_required
 def block(username):
-    admin = Administrator.query.join(Profile).filter(Administrator.profile_id == 
-        current_user.profile_id).first()
+    admin = Administrator.query.join(Profile).filter(Administrator.profile_id ==
+                                                     current_user.profile_id).first()
     if admin is not None:
         user = Profile.query.filter_by(username=username).first()
         user.block = 1
@@ -144,5 +158,3 @@ def block(username):
     else:
         flash("You are not authorised to remove users from Musician's Network. Please contact Administrators")
     return redirect(url_for('main.index'))
-
-
