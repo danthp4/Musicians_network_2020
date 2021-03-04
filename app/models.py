e1898ad82520c445ac08d129c7bb764d25a52c61
Dan Thompson <danielt397@googlemail.com>
2021-03-04 14:32:14 +0000

    Is Working

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db
 
